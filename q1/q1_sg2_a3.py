88984ead1faef29b0504b6e3098111ae3959165d
icelle-aguilar <iaguilar2030@clc.pshs.edu.ph>
2026-08-16 21:48:35 +0800

    Added comments
    Added input validation for birth year and zodiac calculation.

--- q1/q1_sg2_a3.py
+++ q1/q1_sg2_a3.py
@@ -1,6 +1,8 @@
 birthyear = int(input("Enter your birth year: "))
+#Allows the user to input their birth year
 
 if birthyear >= 1900:
+#This is to ensure that if any year before 1900 is inputted, the input will be deemed invalid
   zodiac = ["Rat (鼠 / Shǔ)",
           "Ox (牛 / Niú)",
           "Tiger (虎 / Hǔ)",
@@ -13,7 +15,16 @@ if birthyear >= 1900:
           "Rooster (鸡 / Jī)",
           "Dog (狗 / Gǒu)",
           "Pig (猪 / Zhū)"]
+  #List of the Chinese Zodiacs
+  
   zodiacnum = (birthyear - 1900) % 12
+  '''1900 is subtracted from the birth year because it is our base cutoff.
+  After getting the difference, it is then divided by 12 and the modulo
+  operator is used to get the remainder which will determine the index that
+  the Zodiac is found in.'''
+  
   print(f"Your Chinese Zodiac Sign is : {zodiac[zodiacnum]}")
+  #zodiac -> list, zodiacnum -> item index
+
 else:
   print(f"/n Invalid Year, it should not be earlier than 1900")
