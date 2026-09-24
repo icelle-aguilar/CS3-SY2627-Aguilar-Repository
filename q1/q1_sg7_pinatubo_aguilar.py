6c420d3b0fdf21d06cb35f12eca51e55ab20ea2e
icelle-aguilar <iaguilar2030@clc.pshs.edu.ph>
2026-09-25 07:27:34 +0800

    Update q1_sg7_pinatubo_aguilar.py

--- q1/q1_sg7_pinatubo_aguilar.py
+++ q1/q1_sg7_pinatubo_aguilar.py
@@ -1,2 +1,13 @@
 class Glassware:
-    def __init__(self)
+    def __init__(self, kindofglassware):
+        self.kindofglassware = kindofglassware
+
+
+class Beaker(Glassware):
+    def __init__(self, kindofglassware):
+        super().__init__(kindofglassware)
+
+
+class Tray:
+    def __init__(self):
+        self.beakers = [Beaker() for _ in range(5)]
